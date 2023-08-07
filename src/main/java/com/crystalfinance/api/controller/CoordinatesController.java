e40197886972b450e6b27120a0898c5b1a992204
Fercak Florian <fercak.florian@hotmail.fr>
2023-08-07 21:28:32 +0200

    ajout de la fonctionnalite d'ajout d'une coordonnee

--- src/main/java/com/crystalfinance/api/controller/CoordinatesController.java
+++ src/main/java/com/crystalfinance/api/controller/CoordinatesController.java
@@ -3,6 +3,8 @@ package com.crystalfinance.api.controller;
 import com.crystalfinance.api.model.Coordinates;
 import com.crystalfinance.api.service.CoordinatesService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,5 +21,10 @@ public class CoordinatesController {
     @GetMapping("/")
     public List<Coordinates> getCoordinatesList(){
         return coordinatesService.getCoordinatesList();
+    }
+
+    @PostMapping("/coordinates")
+    public Coordinates addCoordinates(@RequestBody Coordinates coordinates){
+        return coordinatesService.addCoordinates(coordinates);
     }
 }
