2aaee3c796530ff9f1140c45f40cf65696bb30e1
Fercak Florian <fercak.florian@hotmail.fr>
2023-08-07 21:06:41 +0200

    récupération de toutes les coordonnées

--- src/main/java/com/crystalfinance/api/model/Coordinates.java
+++ src/main/java/com/crystalfinance/api/model/Coordinates.java
@@ -1,7 +1,16 @@
 package com.crystalfinance.api.model;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@Document(collection = "coordinates")
 public class Coordinates {
+
+    @Id
+    private String id;
+    private String name;
+    private String latitude;
+    private String longitude;
 }
--- src/main/java/com/crystalfinance/api/service/CoordinatesService.java
+++ src/main/java/com/crystalfinance/api/service/CoordinatesService.java
@@ -1,7 +1,21 @@
 package com.crystalfinance.api.service;
 
+import com.crystalfinance.api.model.Coordinates;
+import com.crystalfinance.api.repository.CoordinatesRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CoordinatesService {
+
+    private CoordinatesRepository coordinatesRepository;
+
+    public CoordinatesService(CoordinatesRepository coordinatesRepository){
+        this.coordinatesRepository = coordinatesRepository;
+    }
+
+    public List<Coordinates> getCoordinatesList(){
+        return coordinatesRepository.findAll();
+    }
 }
