0ce046ce51ea0eaa378a55b8b5e2c59ac77ea07d
Fercak Florian <fercak.florian@hotmail.fr>
2023-08-07 21:52:05 +0200

    ajout fonctionnalite de suppression de coordonnees

--- src/main/java/com/crystalfinance/api/controller/CoordinatesController.java
+++ src/main/java/com/crystalfinance/api/controller/CoordinatesController.java
@@ -2,10 +2,7 @@ package com.crystalfinance.api.controller;
 
 import com.crystalfinance.api.model.Coordinates;
 import com.crystalfinance.api.service.CoordinatesService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +23,10 @@ public class CoordinatesController {
     @PostMapping("/coordinates")
     public Coordinates addCoordinates(@RequestBody Coordinates coordinates){
         return coordinatesService.addCoordinates(coordinates);
+    }
+
+    @DeleteMapping("/coordinates/{id}")
+    public Coordinates deleteCoordinates(@PathVariable("id") String id) {
+        return coordinatesService.deleteCoordinates(id);
     }
 }
--- src/main/java/com/crystalfinance/api/service/CoordinatesService.java
+++ src/main/java/com/crystalfinance/api/service/CoordinatesService.java
@@ -2,10 +2,12 @@ package com.crystalfinance.api.service;
 
 import com.crystalfinance.api.model.Coordinates;
 import com.crystalfinance.api.repository.CoordinatesRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+import java.util.Optional;
+@Slf4j
 @Service
 public class CoordinatesService {
 
@@ -16,10 +18,29 @@ public class CoordinatesService {
     }
 
     public List<Coordinates> getCoordinatesList(){
-        return coordinatesRepository.findAll();
+        List<Coordinates> coordinatesList = coordinatesRepository.findAll();
+        if(coordinatesList.isEmpty()){
+            log.warn("coordinates list is empty");
+            return null;
+        } else {
+            log.info("providing coordinates list");
+            return coordinatesList;
+        }
     }
 
     public Coordinates addCoordinates(Coordinates coordinates){
-        return coordinatesRepository.save(coordinates);
+        return coordinatesRepository.insert(coordinates);
+    }
+
+    public Coordinates deleteCoordinates(String id){
+        Optional<Coordinates> optionalCoordinates = coordinatesRepository.findById(id);
+        if(optionalCoordinates.isEmpty()){
+            log.warn("coordinates not found for this id : " + id);
+            return null;
+        } else {
+            coordinatesRepository.deleteById(id);
+            log.info("coordinates with the id " + id + " is deleted");
+            return optionalCoordinates.get();
+        }
     }
 }
