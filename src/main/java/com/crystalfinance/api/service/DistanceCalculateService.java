8aeee9f01c3fa68f7d079094d762020fc50183f6
Fercak Florian <fercak.florian@hotmail.fr>
2023-08-14 17:28:03 +0200

    amelioration de la classe DistanceCalculateService

--- src/main/java/com/crystalfinance/api/service/DistanceCalculateService.java
+++ src/main/java/com/crystalfinance/api/service/DistanceCalculateService.java
@@ -4,7 +4,9 @@ import com.crystalfinance.api.model.Coordinates;
 import com.crystalfinance.api.repository.CoordinatesRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
 import java.util.Optional;
+
 import net.sf.geographiclib.*;
 
 @Slf4j
@@ -13,23 +15,27 @@ public class DistanceCalculateService {
 
     private CoordinatesRepository coordinatesRepository;
 
-    public DistanceCalculateService(CoordinatesRepository coordinatesRepository){
+    public DistanceCalculateService(CoordinatesRepository coordinatesRepository) {
         this.coordinatesRepository = coordinatesRepository;
     }
 
-    public long calculateDistance(String idOne, String idTwo){
+    public long calculateDistance(String idOne, String idTwo) {
+        long distance = 0;
+        Coordinates coordinatesOne;
+        Coordinates coordinatesTwo;
+
         Optional<Coordinates> optionalCoordinatesOne = coordinatesRepository.findById(idOne);
         Optional<Coordinates> optionalCoordinatesTwo = coordinatesRepository.findById(idTwo);
 
-        Coordinates coordinatesOne = optionalCoordinatesOne.get();
-        Coordinates coordinatesTwo = optionalCoordinatesTwo.get();
+        if (optionalCoordinatesOne.isPresent() && optionalCoordinatesTwo.isPresent()) {
+            coordinatesOne = optionalCoordinatesOne.get();
+            coordinatesTwo = optionalCoordinatesTwo.get();
 
-        GeodesicData geodesic = Geodesic.WGS84.Inverse(coordinatesOne.getLatitude(), coordinatesOne.getLongitude(), coordinatesTwo.getLatitude(), coordinatesTwo.getLongitude());
-
-        double distanceInMeters = geodesic.s12;
-        double distanceInKilometers = distanceInMeters / 1000;
-        long distance = Math.round(distanceInKilometers);
-
+            GeodesicData geodesic = Geodesic.WGS84.Inverse(coordinatesOne.getLatitude(), coordinatesOne.getLongitude(), coordinatesTwo.getLatitude(), coordinatesTwo.getLongitude());
+            double distanceInMeters = geodesic.s12;
+            double distanceInKilometers = distanceInMeters / 1000;
+            distance = Math.round(distanceInKilometers);
+        }
         return distance;
     }
 }
