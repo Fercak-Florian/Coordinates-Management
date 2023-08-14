87924166febb3c3d9d6675bb82b8dc5de342a935
Fercak Florian <fercak.florian@hotmail.fr>
2023-08-14 18:16:07 +0200

    basculement en API REST

--- src/main/java/com/crystalfinance/api/controller/CoordinatesController.java
+++ src/main/java/com/crystalfinance/api/controller/CoordinatesController.java
@@ -5,14 +5,14 @@ import com.crystalfinance.api.service.CoordinatesService;
 import com.crystalfinance.api.service.DistanceCalculateService;
 import com.crystalfinance.api.utils.CoupleOfId;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Slf4j
-@Controller
+@RestController
 public class CoordinatesController {
 
     private long distance = 0;
@@ -20,48 +20,33 @@ public class CoordinatesController {
     private CoordinatesService coordinatesService;
     private DistanceCalculateService distanceCalculateService;
 
-    public CoordinatesController(CoordinatesService coordinatesService, DistanceCalculateService distanceCalculateService){
+    public CoordinatesController(CoordinatesService coordinatesService, DistanceCalculateService distanceCalculateService) {
         this.coordinatesService = coordinatesService;
         this.distanceCalculateService = distanceCalculateService;
     }
 
     @GetMapping("/")
-    public String getCoordinatesList(Model model){
+    public ResponseEntity<List<Coordinates>> getCoordinatesList() {
         List<Coordinates> coordinatesList = coordinatesService.getCoordinatesList();
-        model.addAttribute("coordinatesList", coordinatesList);
-        model.addAttribute("coordinates", new Coordinates());
         log.info("display home page");
-        return "home";
+        return ResponseEntity.ok(coordinatesList);
     }
 
     @PostMapping("/coordinates")
-    public String addCoordinates(@ModelAttribute Coordinates coordinates){
-        coordinatesService.addCoordinates(coordinates);
-        return "redirect:/";
+    public ResponseEntity<Coordinates> addCoordinates(@RequestBody Coordinates coordinates) {
+        Coordinates addedCoordinates = coordinatesService.addCoordinates(coordinates);
+        return ResponseEntity.ok(addedCoordinates);
     }
 
-    @GetMapping("/coordinates/{id}")
-    public String deleteCoordinates(@PathVariable("id") String id) {
-        coordinatesService.deleteCoordinates(id);
-        return "redirect:/";
+    @DeleteMapping("/coordinates/{id}")
+    public ResponseEntity deleteCoordinates(@PathVariable("id") String id) {
+        Coordinates deletedCoordinates = coordinatesService.deleteCoordinates(id);
+        return ResponseEntity.ok(deletedCoordinates);
     }
 
     @PostMapping("/couple-of-id")
-    public String getCoupleOfId(@ModelAttribute CoupleOfId coupleOfId){
+    public ResponseEntity<Long> getCoupleOfId(@RequestBody CoupleOfId coupleOfId) {
         distance = distanceCalculateService.calculateDistance(coupleOfId.getIdOne(), coupleOfId.getIdTwo());
-        return "redirect:/result";
-    }
-
-    @GetMapping("/result")
-    public String displayResult(Model model){
-        if(distance > 10){
-            model.addAttribute("distance", distance);
-            log.info("display the result on 'more' page");
-            return "more";
-        } else{
-            model.addAttribute("distance", distance);
-            log.info("display the result on 'less' page");
-            return "less";
-        }
+        return ResponseEntity.ok(distance);
     }
 }
