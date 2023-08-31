d2a97de180dea56be7a59451abcfc6be0a4165d1
Fercak Florian <fercak.florian@hotmail.fr>
2023-08-31 10:59:33 +0200

    ajout des modifications concernant app.component + app.module

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CoordinatesListComponent } from './coordinates-list/coordinates-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoordinatesListComponent
+    CoordinatesListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
