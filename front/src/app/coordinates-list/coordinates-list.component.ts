a517eb4ab5d3349d08aa3519b40793262b29a8e5
Fercak Florian <fercak.florian@hotmail.fr>
2023-09-01 21:52:35 +0200

    recuperation des coordonnees via le backend java

---
+++ front/src/app/coordinates-list/coordinates-list.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import {CoordinatesService} from "../service/coordinates.service";
+import {Coordinates} from "../models/coordinates.models";
+import {Observable} from "rxjs";
+import {logMessages} from "@angular-devkit/build-angular/src/tools/esbuild/utils";
+
+@Component({
+  selector: 'app-coordinates-list',
+  templateUrl: './coordinates-list.component.html',
+  styleUrls: ['./coordinates-list.component.scss']
+})
+export class CoordinatesListComponent implements OnInit {
+
+  coordinates$!: Observable<Coordinates[]>;
+
+  /*Pour utiliser un service, on l'instantie grâce au constructor*/
+  /*Ici on onject coordinatesService dans la classe CoordinatesListComponent*/
+  constructor(private coordinatesService: CoordinatesService) {
+  }
+
+  /*La méthode  ngOnInit()  est appelée automatiquement par Angular au moment
+   de la création de chaque instance du component.
+   Elle permet notamment d'initialiser des propriétés.*/
+  ngOnInit(): void {
+        this.coordinates$ = this.coordinatesService.getCoordinates();
+        /*Pour être sur que je récupère bien les coordonnées du backend*/
+        console.log(this.coordinates$.forEach(value => console.log(value)));
+    }
+}
