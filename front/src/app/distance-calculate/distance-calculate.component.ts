6506bd99dd96cfa85c2583c494205c795eaf4a2a
Fercak Florian <fercak.florian@hotmail.fr>
2023-09-10 16:15:10 +0200

    recuperation des id des valeurs selectionnees dans les 2 menus deroulants

--- front/src/app/distance-calculate/distance-calculate.component.ts
+++ front/src/app/distance-calculate/distance-calculate.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {CoordinatesService} from "../service/coordinates.service";
 import {Observable, pipe} from "rxjs";
 import {Coordinates} from "../models/coordinates.models";
+import {FormBuilder, FormGroup, NgModel} from "@angular/forms";
 
 @Component({
   selector: 'app-distance-calculate',
@@ -11,15 +12,24 @@ import {Coordinates} from "../models/coordinates.models";
 export class DistanceCalculateComponent implements OnInit {
 
   coordinates$!: Observable<Coordinates[]>;
+  // coupleOfId!: FormGroup;
+  idOne!: string;
+  idTwo!: string;
 
-  constructor(private coordinatesService: CoordinatesService) {
+
+  constructor(private coordinatesService: CoordinatesService, private formBuilder: FormBuilder) {
   }
 
   ngOnInit(): void {
     this.coordinates$ = this.coordinatesService.getCoordinates();
+    /*this.coupleOfId = this.formBuilder.group({
+      idOne: [null],
+      idTwo: [null]
+    });*/
   }
 
   onCalculate(): void {
-
+    console.log(this.idOne);
+    console.log(this.idTwo);
   }
 }
