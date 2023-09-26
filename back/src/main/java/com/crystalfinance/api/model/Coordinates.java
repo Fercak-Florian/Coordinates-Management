0d975648bab4ccf194aca2500f84070fbbae83bb
Fercak Florian <fercak.florian@hotmail.fr>
2023-09-26 19:37:44 +0200

    annotations de validations ajoutees aux champs

--- back/src/main/java/com/crystalfinance/api/model/Coordinates.java
+++ back/src/main/java/com/crystalfinance/api/model/Coordinates.java
@@ -1,5 +1,7 @@
 package com.crystalfinance.api.model;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -16,8 +18,15 @@ public class Coordinates {
     }
 
     @Id
+    @NotEmpty(message = "id field cannot be empty")
     private String id;
+
+    @NotEmpty(message = "name field cannot be empty")
     private String name;
+
+    @NotEmpty(message = "latitude field cannot be empty")
     private double latitude;
+
+    @NotEmpty(message = "longitude field cannot be empty")
     private double longitude;
 }
